0892e4463926faefdc688f795a6d20fa84b2e9f1
Chloe Alieva <chloe@email.com>
2022-07-12 18:11:33 +0300

    Расчет бонусных миль v1.0

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+        public static void main(String[] args) {
+            int price = 9999 ;
+            int malePrice = 20 ;
+            int mile = price / malePrice ;
+
+            System.out.println("За покупку начислится " + mile + " миль(и)");
+
+    
+    }
+  }
