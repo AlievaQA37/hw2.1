fb13b4dddbba3d53d1a137ed31e91bb5bb056912
Chloe Alieva <chloe@email.com>
2022-07-15 17:43:54 +0300

    Revert "Расчет бонусных миль v3.0"
    This reverts commit ed5415f4a93a301dc41e27b81c11646742623404.

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
         public static void main(String[] args) {
-                int price = 9999;
-                int milePrice = 20;
-                int mile = price / milePrice;
+            int price = 9999 ;
+            int milePrice = 20 ;
+            int mile = price / milePrice ;
 
-                System.out.println("За покупку начислится " + mile + " миль(и)");
-        }
-}
+            System.out.println("За покупку начислится " + mile + " миль(и)");
+
+    
+    }
+  }
